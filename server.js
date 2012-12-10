ce5d427026de65a07d431a170a52063cf7740474
Moritz Guth <m.guth@de.edenspiekermann.com>
2012-12-10 14:22:21 +0100

    switch from ejs to hamljs for templating

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require('express'),
+    haml = require('hamljs'),
+    cons   = require('consolidate'),
     handler = require('./handler.js'),
     littleprinter = require('littleprinter');
 
 var app = express();
 var port = process.env.PORT || 5000;
 
-app.set('view engine', 'ejs'); // in this example I am using ejs, feel free to use a different view engine if you wish
+// Make HAML.js work (with the help of consolidate)
+app.engine('haml', cons.haml);
+app.set('view engine', 'haml');
 
 littleprinter.setup(app, handler);
 
