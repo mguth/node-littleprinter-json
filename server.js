d5a6831c7b33bf15cae811c5dd4547871184329f
Moritz Guth <m.guth@de.edenspiekermann.com>
2012-12-11 16:45:39 +0100

    adjust server settings

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ var port = process.env.PORT || 5000;
 // Use Jade as templating language
 app.set('view engine', 'jade');
 
+// Use path prefix to declare static files
+app.use('/static', express.static(__dirname + '/public'));
+// Provide proper index page
+app.get('/', function(req,res){
+    res.render('index', {layout: false});
+});
 littleprinter.setup(app, handler);
 
 app.listen(port);
