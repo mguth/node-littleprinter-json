9e50bde9bf0571a1518fd53d3cd2aed771055807
Moritz Guth <m.guth@de.edenspiekermann.com>
2012-12-15 22:46:18 +0100

    Steamline datafeed module, add event emitter, update server and template

--- datafeed.js
+++ datafeed.js
@@ -1,3 +1,4 @@
+var event = require( './event' );
 var http = require('http');
 
 // Content source configuration
@@ -7,39 +8,30 @@ var contentSource = {
   path: '/api/2/sites/what-i-ate/posts/public'
 };
 
-function Datafeed() {
-  if(false === (this instanceof Datafeed)) {
-      return new Datafeed();
-  }
-}
+var jsonContent = "";
 
-Datafeed.prototype.download = function() {
-
-  var jsonContent = "";
-
-  // Start request and get the data
-  var req = http.request(contentSource, function(res) {
-    console.log('Remote server response: ' + res.statusCode);
-    res.setEncoding('utf8');
-    res.on('data', function (chunk) {
-      jsonContent += chunk;
-    });
+// Start request and get the data
+var req = http.request(contentSource, function(res) {
+  console.log('Remote server response: ' + res.statusCode);
+  res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+    jsonContent += chunk;
   });
+});
 
-  // Show errors
-  req.on('error', function(e) {
-    console.log('Problem with request: ' + e.message);
-  });
+// Show errors
+req.on('error', function(e) {
+  console.log('Problem with request: ' + e.message);
+});
 
-  // The reading is finished, let's parse and return the result
-  req.on('close', function() {
-    jsonContent = JSON.parse(jsonContent);
-    console.log("Import complete: " + jsonContent.length + " items imported from JSON.");
-    return jsonContent;
-  });
+// The reading is finished, let's parse and return the result
+req.on('close', function() {
+  jsonContent = JSON.parse(jsonContent);
+  console.log("Import complete: " + jsonContent.length + " items imported from JSON.");
+  exports.jsonContent = jsonContent;
+  event.emit( 'import_complete' );
+});
 
-  // Close the request
-  req.end();
-};
+// Close the request
+req.end();
 
-module.exports = Datafeed;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var express = require('express'),
 
 var app = express();
 var port = process.env.PORT || 5000;
+var event = require( './event' );
 
 // Use Jade as templating language
 app.set('view engine', 'jade');
@@ -12,23 +13,17 @@ app.set('view engine', 'jade');
 // Use path prefix to declare static files
 app.use('/static', express.static(__dirname + '/public'));
 
-// Acquire JSON content from external source
-var Datafeed = require('./datafeed.js');
-var posterousJson = new Datafeed();
-var importedData = posterousJson.download();
-
-// Push acquired JSON content to templates before rendering
-var beforeRender = function(req, res, next) {
-  res.locals.jsonData = importedData;
-  console.log('Imported Data in server.js: ' + importedData);
-  return next();
-};
-app.use(beforeRender);
-
-// Provide proper index page
-app.get('/', function(req,res){
-    res.render('index', {layout: false});
+// Acquire JSON content from external source and make it available in templates
+var datafeed = require('./datafeed.js');
+event.on( 'import_complete', function(){
+  app.locals.jsonContent = datafeed.jsonContent;
+  // console.log('Imported in server.js: ' + datafeed.jsonContent.length + ' items');
 });
+
+// Provide proper index page (disabled b/c of collision with littleprinter module)
+// app.get('/', function(req,res){
+//     res.render('index', {layout: false});
+// });
 
 littleprinter.setup(app, handler);
 
